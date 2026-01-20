53c2361d551d35962bfb4fc7041b5f50db6dbcd8
302-23\koyo <kobe-license@kobe-tech.ac.jp>
2026-01-20 18:29:15 +0900

    キャラクターデータ

---
+++ program/Data/characterdata.h
@@ -0,0 +1,72 @@
+#pragma once
+#include <string>
+
+enum class Faction {
+	Player,
+	Enemy
+};
+
+struct Status {
+	std::string name;
+
+	Faction faction;
+
+	bool isLife = true;
+
+	int ID;
+	int Lv = 1;
+	int Exp = 0;
+	int MaxHP = 50;
+	int hp = MaxHP;
+
+	int ATK;
+	int DEF;
+	int INT;
+	int MND;
+
+	int actionPoint;
+	int speed;
+	int actionValue;
+};
+
+
+
+class Character {
+protected:
+	Status state;
+
+public:
+
+	Character(Status baseStatas) :state(baseStatas){}
+
+	~Character(){}
+
+	/// <summary>
+	/// ファクションゲッター
+	/// </summary>
+	/// <returns></returns>
+	Faction GetFaction()const { return state.faction; }
+
+	const Status& GetState()const { return state; }
+
+	bool IsLife() { return state.isLife; }
+
+	void TakeDamage(int damage) {
+		if (damage < 0)damage = 0;
+		state.hp -= damage;
+		if (state.hp < 0)
+		{
+			state.hp = 0;
+			state.isLife = false;
+		}
+	}
+
+	void Heal(int value)
+	{
+		if (value < 0) { return; }
+		state.hp += value;
+		if (state.hp > state.MaxHP) {
+			state.hp = state.MaxHP;
+		}
+	}
+};
