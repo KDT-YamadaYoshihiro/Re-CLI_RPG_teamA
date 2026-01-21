d9c7c96afd4978cea8219aaee8c64be1eff3bec3
302-24\koyo <kobe-license@kobe-tech.ac.jp>
2026-01-21 17:44:43 +0900

    シングルトン、シーンマネージャーを作成

--- program/Singleton/Singleton.h
+++ program/Singleton/Singleton.h
@@ -1,2 +1,24 @@
 #pragma once
 
+template <typename T>
+// 継承して使えるSingletonヘッダー
+class Singleton {
+public:
+    // インスタンスの取得
+    static T& Instance()
+    {
+        static T instance;
+        return instance;
+    }
+    // コピーコンストラクタと代入演算子を禁止
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+    // ムーブも禁止
+    Singleton(Singleton&&) = delete;
+    // ムーブ代入も禁止
+    Singleton& operator=(Singleton&&) = delete;
+protected:  // 継承先でも使えるようにprotected:
+    Singleton() = default;
+    // 仮想デストラクタ
+    virtual ~Singleton() = default;
+};
