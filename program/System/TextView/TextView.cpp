d33cfde82da3ad4c4ef05c17c862ebbce5d5abce
yamada <KDT24GPR30005@edu.kobe-tech.ac.jp>
2026-01-29 10:02:22 +0900

    連続的に描画が走らないようにフラグを設定。

--- program/System/TextView/TextView.cpp
+++ program/System/TextView/TextView.cpp
@@ -3,15 +3,23 @@
 
 void TextView::AddText(std::string arg_text)
 {
-	m_text += arg_text;
+	if (m_text == arg_text) {
+		return;
+	}
+	m_text = arg_text;
+	viewFlag = true;
 }
 
 void TextView::ResetText()
 {
 	m_text.clear();
+	system("cls");
 }
 
 void TextView::TextRender()
 {
-	std::cout << m_text << std::endl;
+	if (viewFlag) {
+		std::cout << m_text << std::endl;
+		viewFlag = false;
+	}
 }
--- program/System/TextView/TextView.h
+++ program/System/TextView/TextView.h
@@ -10,23 +10,21 @@ class TextView : public Singleton<TextView>
 
 	TextView() = default;
 	virtual ~TextView() = default;
+	// 表示するテキスト
+	std::string m_text;
+	// 描画Flag
+	bool viewFlag = true;
 
 public:
 
 	// テキスト内容の追加
 	void AddText(std::string arg_text);
 
-	// テキスト内容の削除
+	// 画面のクリア、テキスト内容のリセット
 	void ResetText();
 
 	// テキスト内容の表示
 	void TextRender();
-
-
-private:
-
-	// 表示するテキスト
-	std::string m_text;
 
 };
 
