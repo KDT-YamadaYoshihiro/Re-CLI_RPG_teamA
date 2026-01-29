5b6dd7a0ee4f309cc348b6f564e5dcfb99d5c152
yamada <KDT24GPR30005@edu.kobe-tech.ac.jp>
2026-01-29 17:12:16 +0900

    修正

--- program/System/TextView/TextView.h
+++ program/System/TextView/TextView.h
@@ -2,10 +2,6 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#ifdef _WIN32
-#include <Windows.h>
-#endif // _WIN32
-
 #include "../../Singleton/Singleton.h"
 
 class TextView : public Singleton<TextView>
