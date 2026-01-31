daf6f88c96fb35148210f3d6cc1015f1058a12b2
sakanaba <kdt24sgc40003@edu.kobe-tech.ac.jp>
2026-02-01 07:02:52 +0900

    戦闘部分マネージャー、敵と味方のマネージャー、インプットのバグを修正

--- program/Data/Party/PartyMG.h
+++ program/Data/Party/PartyMG.h
@@ -5,10 +5,23 @@
 #define PARTY_MAX 4
 class Character;
 
-class PartyMG
-{
+class PartyMG {
 private:
+    std::vector<std::unique_ptr<Character>> members;
+    //共通ポイント
+    int sharedSP = 3; 
+
 public:
-	void Init();
-	const std::vector<std::unique_ptr<Character>>& GetParty() const;
+    // キャラの追加
+    void AddMember(std::unique_ptr<Character> chara) { members.push_back(std::move(chara)); }
+
+    // 生存しているメンバーのリストを返す
+    std::vector<Character*> GetActiveMembers() {
+        std::vector<Character*> actives;
+        for (auto& m : members) if (m->IsLife()) actives.push_back(m.get());
+        return actives;
+    }
+
+    int GetSP() const { return sharedSP; }
+    void UseSP(int cost) { sharedSP -= cost; }
 };
